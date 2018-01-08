e14412b46ed25c9386d3adb24160fd07abc9ccb6
sharme <yaomiwang@qq.com>
2018-01-08 23:36:31 +0800

    fix getNews does have n_avatar

--- trip_server/routes/news.js
+++ trip_server/routes/news.js
@@ -34,7 +34,8 @@ router.post('/add',  function (req, res, next) {
 
 router.get('/getNews',  function (req, res, next) {
 
-    var SQL = mysql.format("select n_id, (select u_name from users where u_id = nw.n_from ) as n_from," +
+    var SQL = mysql.format("select n_id, (select u_avatar from users where u_id = nw.n_from) as n_avatar," +
+        " (select u_name from users where u_id = nw.n_from ) as n_from," +
         "(select u_name from users where u_id = nw.n_to) as n_to, n_type, n_content, n_created_time, n_created_by, n_updated_time, n_updated_by from news as nw where nw.n_to = ?", [req.param('n_to')]);
 
     connection.query(SQL, function (err, result) {
